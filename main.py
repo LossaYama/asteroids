483c8b5226dfc10e1b0c530a4c4806d72f6681ad
LossaYama <alyssarsch@gmail.com>
2026-05-07 18:39:58 +0100

    player turning and import cleanup

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
-    dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pass
+
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
