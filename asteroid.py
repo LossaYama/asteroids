f39276bdc77adaac5b27aab11b69a5c52dfabd61
LossaYama <alyssarsch@gmail.com>
2026-05-07 20:47:44 +0100

    plans for the future in main

--- asteroid.py
+++ asteroid.py
@@ -27,3 +27,17 @@ class Asteroid(CircleShape):
         baby_two = Asteroid(self.position[0], self.position[1], baby_radius)
         baby_one.velocity = baby_one_vector * 1.2
         baby_two.velocity = baby_two_vector * 1.2
+
+#THOUGHTS FOR FUTURE
+#Add a scoring system
+#Implement multiple lives and respawning
+#Add an explosion effect for the asteroids
+#Add acceleration to the player movement
+#Make the objects wrap around the screen instead of disappearing
+#Add a background image
+#Create different weapon types
+#Make the asteroids lumpy instead of perfectly round
+#Make the ship have a triangular hit box instead of a circular one
+#Add a shield power-up
+#Add a speed power-up
+#Add bombs that can be dropped
